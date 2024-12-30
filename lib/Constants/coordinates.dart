3cf56b1a841812bebfde6cdab5db53e29bce3bcc
LivramentoAmaral <marcosdoamaral2002@gmail.com>
2024-12-30 17:08:04 -0300

    atualização na geolocation do ônibus

---
+++ lib/Constants/coordinates.dart
@@ -0,0 +1,69 @@
+import 'package:latlong2/latlong.dart';
+
+final List<Map<String, dynamic>> coordinates = [
+  {
+    "location": const LatLng(-10.43029, -45.174006),
+    "tooltip": "TRAJETO SAÍDA DO IFPI",
+    "popup": "IFPI - Campus Corrente"
+  },
+  {
+    "location": const LatLng(-10.438866, -45.173230),
+    "tooltip": "1ª Parada",
+    "popup": "Posto Primavera"
+  },
+  {
+    "location": const LatLng(-10.454374, -45.171460),
+    "tooltip": "2ª Parada",
+    "popup": "Praça Principal do Vermelhão"
+  },
+  {
+    "location": const LatLng(-10.439672, -45.168913),
+    "tooltip": "3ª Parada",
+    "popup": "Supermercado Rocha"
+  },
+  {
+    "location": const LatLng(-10.443239, -45.160735),
+    "tooltip": "4ª Parada",
+    "popup": "Praça da Igreja Batista"
+  },
+  {
+    "location": const LatLng(-10.445506, -45.157068),
+    "tooltip": "5ª Parada",
+    "popup": "15ª Regional de Educação"
+  },
+  {
+    "location": const LatLng(-10.451376, -45.146146),
+    "tooltip": "6ª Parada",
+    "popup": "Posto de Combustível do Aeroporto"
+  },
+  {
+    "location": const LatLng(-10.454766, -45.137556),
+    "tooltip": "7ª Parada",
+    "popup": "Escola Municipal Orley Cavalcante Pacheco"
+  },
+  {
+    "location": const LatLng(-10.442140, -45.158292),
+    "tooltip": "8ª Parada",
+    "popup": "APAEB Corrente"
+  },
+  {
+    "location": const LatLng(-10.439274, -45.162402),
+    "tooltip": "9ª Parada",
+    "popup": "SAMU Corrente"
+  },
+  {
+    "location": const LatLng(-10.436220, -45.162002),
+    "tooltip": "10ª Parada",
+    "popup": "Próximo ao Coronel"
+  },
+  {
+    "location": const LatLng(-10.429860, -45.161559),
+    "tooltip": "11ª Parada",
+    "popup": "Próximo ao posto Varejão"
+  },
+  {
+    "location": const LatLng(-10.43029, -45.174006),
+    "tooltip": "12ª Parada",
+    "popup": "Retorno ao IFPI"
+  },
+];
--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http; // Importando para fazer requisições HTTP
+import 'package:acesstrans/core/theme/app_themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_tts/flutter_tts.dart';
@@ -5,7 +8,7 @@ import 'package:latlong2/latlong.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
-import 'package:acesstrans/core/theme/app_themes.dart';
+import 'package:acesstrans/Constants/coordinates.dart'; // Importando as coordenadas
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,85 +18,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<Map<String, dynamic>> coordinates = [
-    {
-      "location": const LatLng(-10.43029, -45.174006),
-      "tooltip": "TRAJETO SAÍDA DO IFPI",
-      "popup": "IFPI - Campus Corrente"
-    },
-    {
-      "location": const LatLng(-10.438866, -45.173230),
-      "tooltip": "1ª Parada",
-      "popup": "Posto Primavera"
-    },
-    {
-      "location": const LatLng(-10.454374, -45.171460),
-      "tooltip": "2ª Parada",
-      "popup": "Praça Principal do Vermelhão"
-    },
-    {
-      "location": const LatLng(-10.439672, -45.168913),
-      "tooltip": "3ª Parada",
-      "popup": "Supermercado Rocha"
-    },
-    {
-      "location": const LatLng(-10.443239, -45.160735),
-      "tooltip": "4ª Parada",
-      "popup": "Praça da Igreja Batista"
-    },
-    {
-      "location": const LatLng(-10.445506, -45.157068),
-      "tooltip": "5ª Parada",
-      "popup": "15ª Regional de Educação"
-    },
-    {
-      "location": const LatLng(-10.451376, -45.146146),
-      "tooltip": "6ª Parada",
-      "popup": "Posto de Combustível do Aeroporto"
-    },
-    {
-      "location": const LatLng(-10.454766, -45.137556),
-      "tooltip": "7ª Parada",
-      "popup": "Escola Municipal Orley Cavalcante Pacheco"
-    },
-    {
-      "location": const LatLng(-10.442140, -45.158292),
-      "tooltip": "8ª Parada",
-      "popup": "APAEB Corrente"
-    },
-    {
-      "location": const LatLng(-10.439274, -45.162402),
-      "tooltip": "9ª Parada",
-      "popup": "SAMU Corrente"
-    },
-    {
-      "location": const LatLng(-10.436220, -45.162002),
-      "tooltip": "10ª Parada",
-      "popup": "Próximo ao Coronel"
-    },
-    {
-      "location": const LatLng(-10.429860, -45.161559),
-      "tooltip": "11ª Parada",
-      "popup": "Próximo ao posto Varejão"
-    },
-    {
-      "location": const LatLng(-10.43029, -45.174006),
-      "tooltip": "12ª Parada",
-      "popup": "Retorno ao IFPI"
-    },
-  ];
-
   FlutterTts flutterTts = FlutterTts();
   final stt.SpeechToText _speechToText = stt.SpeechToText();
   bool _isListening = false;
   bool _speechEnabled = false;
   String _lastWords = '';
+  LatLng _busLocation = LatLng(-10.43029, -45.174006); // Localização inicial do ônibus
 
   @override
   void initState() {
     super.initState();
     _initSpeech();
   }
+
+  // Função para pegar a localização do ônibus via API
+  Future<void> _fetchBusLocation() async {
+  final response = await http.get(Uri.parse('https://api-location-teal.vercel.app/api/location'));
+  
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);  // Deserializa o JSON
+    print('Localização do ônibus recebida: $data');
+    
+    // Acessa a latitude e longitude dentro da chave 'location'
+    double latitude = data["location"]['latitude'];
+    double longitude = data["location"]['longitude'];
+    
+    // Atualiza a localização do ônibus no mapa
+    setState(() {
+      _busLocation = LatLng(latitude, longitude);  // Atualiza a posição no mapa
+    });
+  } else {
+    print('Falha ao carregar a localização do ônibus');
+  }
+}
+
 
   void _initSpeech() async {
     await _requestMicrophonePermission();
@@ -195,14 +153,13 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: FlutterMap(
-        options: const MapOptions(
-          initialCenter: LatLng(-10.43029, -45.174006),
+        options: MapOptions(
+          initialCenter: _busLocation,
           initialZoom: 13.0,
         ),
         children: [
           TileLayer(
-            urlTemplate:
-                "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+            urlTemplate: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
             subdomains: ['a', 'b', 'c'],
           ),
           MarkerLayer(
@@ -233,8 +190,7 @@ class _HomePageState extends State<HomePage> {
                             style: TextStyle(
                               fontSize: 24,
                               fontWeight: FontWeight.bold,
-                              color:
-                                  Theme.of(context).textTheme.bodyLarge?.color,
+                              color: Theme.of(context).textTheme.bodyLarge?.color,
                             ),
                             textAlign: TextAlign.center,
                           ),
@@ -242,8 +198,7 @@ class _HomePageState extends State<HomePage> {
                             coord['popup'],
                             style: TextStyle(
                               fontSize: 20,
-                              color:
-                                  Theme.of(context).textTheme.bodyMedium?.color,
+                              color: Theme.of(context).textTheme.bodyMedium?.color,
                             ),
                             textAlign: TextAlign.center,
                           ),
@@ -252,8 +207,7 @@ class _HomePageState extends State<HomePage> {
                               child: ElevatedButton(
                                 onPressed: () => Navigator.of(context).pop(),
                                 style: ElevatedButton.styleFrom(
-                                  backgroundColor:
-                                      Theme.of(context).primaryColor,
+                                  backgroundColor: Theme.of(context).primaryColor,
                                   shape: RoundedRectangleBorder(
                                     borderRadius: BorderRadius.circular(30),
                                   ),
@@ -271,9 +225,33 @@ class _HomePageState extends State<HomePage> {
                   },
                 ),
               );
-            }).toList(),
+            }).toList()
+              ..add(
+                Marker(
+                  point: _busLocation,
+                  width: 50.0,
+                  height: 50.0,
+                  child: IconButton(
+                    icon: Image.network(
+                      'https://cdn-icons-png.flaticon.com/512/3448/3448339.png',
+                      width: 40,
+                      height: 40,
+                    ),
+                    onPressed: () {
+                      _speak("A localização do ônibus foi atualizada.");
+                    },
+                  ),
+                ),
+              ),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _fetchBusLocation(); // Atualiza a localização do ônibus
+        },
+        child: Icon(Icons.location_on),
+        backgroundColor: AppThemes.lightTheme.primaryColor,
       ),
     );
   }
